770aa120ff71d481121e737b86c4e3bb0b413081
ericmustin <mustin.eric@gmail.com>
2019-02-07 00:03:50 +0100

    take a base64screenshot thats broken

--- background.js
+++ background.js
@@ -1,32 +1,36 @@
 'use strict';
 
 (function() {
-  // var ga = document.createElement('script'); ga.type =
-'text/javascript'; ga.async = true;
-  // ga.src = 'https://ssl.google-analytics.com/ga.js';
-  // var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ga, s);
-  console.log('hello werld2');
-
-
   function captureWindowTab(tabid) {
-    chrome.tabs.update(tabid, {}, function() {
-      chrome.tabs.captureVisibleTab(27, {format:"png"}, function(dataUrl) {
+    // chrome.tabs.update(tabid, {}, function() {
+      chrome.tabs.captureVisibleTab(tabid, {format:"png"}, function(dataUrl) {
         console.log('in here');
         console.log(dataUrl);
       });
-    });
+    // });
   }
 
   var getDetails = function() {
     var result = document.getElementById("website")
     console.log(result.value)
     var newURL = result.value;
-    chrome.tabs.create({ url: newURL, active: false }, function(tab) {
-       result = captureWindowTab(tab.id);
-       console.log(result)
+    chrome.tabs.create({ url: newURL, active: false }, function(tab,anyinfo) {
+    	window.setTimeout(function(){
+
+	    	// chrome.tabs.onUpdated.addListener(function(tabId, info) {
+
+	    	// })
+        console.log(tab,anyinfo)
+	      chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, {format:"png"}, function(dataUrl) {
+	        console.log('in here');
+	        alert(dataUrl);
+	      });	    	
+	       // result = captureWindowTab(tab.id);
+	       // console.log(result)	
+    	},4000)
+    	
     });
-  }
+  };
 
   document.getElementById("website_button").onclick = getDetails;
 })();
