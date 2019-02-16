d6a3462e88891a4530b1b206eb73ba248d602649
ericmustin <mustin.eric@gmail.com>
2019-02-16 16:26:24 +0100

    color selection options wireframe

--- background_two.js
+++ background_two.js
@@ -2,14 +2,20 @@
 
 (function() {
   var getDetails = function() {
+    var options_map = ["color_csv","color_url"]
+    var selected_object, selected_value, selected_input;
+    
+    
 
-    console.log('this is executed')
-    var result = document.getElementById("website")
-    console.log(result.value)
-    var newURL = result.value;
+    selected_object = document.getElementById("selected_type")
+    selected_value = selected_object.options[selected_object.selectedIndex].value
+    selected_input = document.getElementById(options_map[selected_value-1]).value
+
+    console.log(selected_input)
+    var newURL = selected_input;
     
     
-    chrome.runtime.sendMessage({ result: true, data: newURL });
+    chrome.runtime.sendMessage({ result: true, data: selected_input });
   };
 
   document.getElementById("website_button").onclick = getDetails;
