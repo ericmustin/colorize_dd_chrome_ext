8f7d1cfce592d4afcede149bc3fa644fd3a80a7e
ericmustin <mustin.eric@gmail.com>
2019-02-17 12:54:16 +0100

    clean up and format to use hex code option

--- form_submit_script.js
+++ form_submit_script.js
@@ -2,20 +2,19 @@
 
 (function() {
   var getDetails = function() {
+    // map btwn <select> selected_type options and <input> form_type values
     var options_map = ["color_csv","color_url"]
+
     var selected_object, selected_value, selected_input;
-    
-    
 
     selected_object = document.getElementById("selected_type")
     selected_value = selected_object.options[selected_object.selectedIndex].value
     selected_input = document.getElementById(options_map[selected_value-1]).value
+    
 
-    console.log(selected_input)
-    var newURL = selected_input;
-    
-    
+    // do validation of hex vs url in content script
     chrome.runtime.sendMessage({ result: true, data: selected_input });
+
   };
 
   document.getElementById("website_button").onclick = getDetails;
